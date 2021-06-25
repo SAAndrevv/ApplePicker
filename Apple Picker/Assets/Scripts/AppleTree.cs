be7954257350746d88c12f0f762e94f50b37c550
Andrey Sudomoykin <SAAndrew52@yandex.ru>
2021-06-25 11:38:11 +0300

    Init commit

---
+++ Apple Picker/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppleTree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 20f;
+    public float chanceToChangeDirection = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        DropApple();
+    }
+
+    private void FixedUpdate() {
+        if (Random.value < chanceToChangeDirection){
+            speed *= -1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge){
+            speed = Mathf.Abs(speed);
+        }
+        else if(pos.x > leftAndRightEdge){
+            speed = -Mathf.Abs(speed);
+        }
+        
+    }
+    private void DropApple(){
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    public void On_click(){
+        SceneManager.LoadScene("_Scene_1");
+    }
+}
---
+++ Apple Picker/Assets/Scripts/HightScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HightScore : MonoBehaviour
+{
+    public static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake() {
+        if (PlayerPrefs.HasKey("HightScore")){
+            score = PlayerPrefs.GetInt("HightScore");
+        }
+        PlayerPrefs.SetInt("HightScore", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "Hight Score: " + score;
+        if (score > PlayerPrefs.GetInt("HightScore")){
+            PlayerPrefs.SetInt("HightScore", score);
+        }
+    }
+}
